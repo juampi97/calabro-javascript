739097d87e82e5a1c2fa7ebeb68991bf7a234d15
Juan Pablo Calabro <juampicalabro97@gmail.com>
2022-12-25 22:41:48 -0300

    Funcion boton add

--- js/carritoProyectores.js
+++ js/carritoProyectores.js
@@ -1,6 +1,8 @@
 let proyectoresStock = proyectores.slice(0);
 let proyectoresReservados = [];
 
+let elementosCarrito = 0;
+
 // Evento boton agregar carrito
 
 function generateBTNaddID() {
@@ -8,9 +10,32 @@ function generateBTNaddID() {
   btnAdd.forEach((boton) => {
     boton.addEventListener("click", () => {
       let elementos = boton.id.split("-");
-      btnAddID = elementos[1]
+      let btnAddID = elementos[1];
       console.log(btnAddID);
+      elementosCarrito++;
+      console.log(elementosCarrito);
+      actulizarBTNCarrito(elementosCarrito);
+      actualizarProyectoresStock(btnAddID);
     });
   });
 }
+
+function actulizarBTNCarrito(elementos){
+  let itemsCarrito = document.querySelector('#burbujaCarrito');
+  if(elementos == 0){
+    itemsCarrito.innerHTML = '';
+    itemsCarrito.class = 'badge rounded-pill bg-warning d-none'
+  }else{
+    itemsCarrito.class = 'badge rounded-pill bg-warning'
+    itemsCarrito.innerHTML = `<p>${elementos}</p>`;
+  }
+  }
+
+function actualizarProyectoresStock(item){
+  
+}
+
+  window.addEventListener("load", function () {
+    actulizarBTNCarrito(elementosCarrito);
+  });
 
--- js/catalogoProyectores.js
+++ js/catalogoProyectores.js
@@ -105,57 +105,57 @@ function generateCatalagoGenerico(array) {
 
     if (elemento.hdmi) {
       card.innerHTML += `<h5 class="card-title text-center py-2" id="cardCodRec">
-    ${elemento.cod_rec}
-  </h5>
-  <p class="card-text py-2" id="cardMarca">Marca: ${elemento.marca}</p>
-  <p class="card-text py-2" id="cardModelo">Modelo: ${elemento.modelo}</p>
-  <p class="card-text py-2" id="cardHDMI">HDMI: ${elemento.salidaHDMI()}</p>
-  <p class="card-text py-2" id="cardVGA">VGA: ${elemento.salidaVGA()}</p>
-  <p class="py-2">El maletín incluye alimentación, cabla VGA y zapatilla multitoma.</p>
-       <div class="mb-3 form-check">
-         <input type="checkbox" class="form-check-input" id="adicional_hdmi" value="agregado">
-         <label class="form-check-label" for="exampleCheck1">HDMI (opcional)</label>
-       </div>
-       <div class="mb-2 form-check">
-         <input type="checkbox" class="form-check-input" id="adicional_zapatilla" value="agregado">
-         <label class="form-check-label" for="exampleCheck1">Zapatilla adicional (opcional)</label>
-       </div>
-  <div class="text-center pt-2">
-    <div class="row d-flex">
-      <div class="col boton_reserva">
-        <button type="button" class="btn btn-success btnAdd" id="btnAdd-${
-          elemento.cod_rec
-        }">
-          Agregar
-        </button>
-      </div>
-    </div>
-  </div>`;
+      ${elemento.cod_rec}
+      </h5>
+      <p class="card-text py-2" id="cardMarca">Marca: ${elemento.marca}</p>
+      <p class="card-text py-2" id="cardModelo">Modelo: ${elemento.modelo}</p>
+      <p class="card-text py-2" id="cardHDMI">HDMI: ${elemento.salidaHDMI()}</p>
+      <p class="card-text py-2" id="cardVGA">VGA: ${elemento.salidaVGA()}</p>
+      <p class="py-2">El maletín incluye alimentación, cabla VGA y zapatilla multitoma.</p>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="adicional_hdmi" value="agregado">
+            <label class="form-check-label" for="exampleCheck1">HDMI (opcional)</label>
+          </div>
+          <div class="mb-2 form-check">
+            <input type="checkbox" class="form-check-input" id="adicional_zapatilla" value="agregado">
+            <label class="form-check-label" for="exampleCheck1">Zapatilla adicional (opcional)</label>
+          </div>
+      <div class="text-center pt-2">
+        <div class="row d-flex">
+          <div class="col boton_reserva">
+            <button type="button" class="btn btn-success btnAdd" id="btnAdd-${
+              elemento.cod_rec
+            }">
+              Agregar
+            </button>
+          </div>
+        </div>
+      </div>`;
     } else {
-      card.innerHTML += `<h5 class="card-title text-center py-2" id="cardCodRec">
-    ${elemento.cod_rec}
-  </h5>
-  <p class="card-text py-2" id="cardMarca">Marca: ${elemento.marca}</p>
-  <p class="card-text py-2" id="cardModelo">Modelo: ${elemento.modelo}</p>
-  <p class="card-text py-2" id="cardHDMI">HDMI: ${elemento.salidaHDMI()}</p>
-  <p class="card-text py-2" id="cardVGA">VGA: ${elemento.salidaVGA()}</p>
-  <p class="py-2">El maletín incluye alimentación, cabla VGA y zapatilla multitoma.</p>
-       
-       <div class="mb-5 form-check">
-         <input type="checkbox" class="form-check-input" id="adicional_zapatilla" value="agregado">
-         <label class="form-check-label" for="exampleCheck1">Zapatilla adicional (opcional)</label>
-       </div>
-  <div class="text-center pt-2">
-    <div class="row d-flex">
-      <div class="col boton_reserva">
-        <button type="button" class="btn btn-success btnAdd" id="btnAdd-${
-          elemento.cod_rec
-        }">
-          Agregar
-        </button>
-      </div>
-    </div>
-  </div>`;
+          card.innerHTML += `<h5 class="card-title text-center py-2" id="cardCodRec">
+        ${elemento.cod_rec}
+      </h5>
+      <p class="card-text py-2" id="cardMarca">Marca: ${elemento.marca}</p>
+      <p class="card-text py-2" id="cardModelo">Modelo: ${elemento.modelo}</p>
+      <p class="card-text py-2" id="cardHDMI">HDMI: ${elemento.salidaHDMI()}</p>
+      <p class="card-text py-2" id="cardVGA">VGA: ${elemento.salidaVGA()}</p>
+      <p class="py-2">El maletín incluye alimentación, cabla VGA y zapatilla multitoma.</p>
+          
+          <div class="mb-5 form-check">
+            <input type="checkbox" class="form-check-input" id="adicional_zapatilla" value="agregado">
+            <label class="form-check-label" for="exampleCheck1">Zapatilla adicional (opcional)</label>
+          </div>
+      <div class="text-center pt-2">
+        <div class="row d-flex">
+          <div class="col boton_reserva">
+            <button type="button" class="btn btn-success btnAdd" id="btnAdd-${
+              elemento.cod_rec
+            }">
+              Agregar
+            </button>
+          </div>
+        </div>
+      </div>`;
     }
 
     catalogo.append(card);
