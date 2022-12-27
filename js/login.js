9e7d86438eb4ad59c7614549d0ce6e8d3548b82a
Juan Pablo Calabro <juampicalabro97@gmail.com>
2022-12-27 13:04:20 -0300

    login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,8 @@
+// Variable que indique si el usuario se logeo o no
+
+let usuarioLogeado = false;
+sessionStorage.setItem("usuarioLogeado", usuarioLogeado);
+
 // Traigo elementos del DOM - Modal
 let userLogin = document.getElementById("userLogin");
 let passLogin = document.getElementById("passLogin");
@@ -6,12 +11,12 @@ let btnLogout = document.getElementById("btnLogout");
 let modalLogin = document.getElementById("modalLogin");
 let modal = new bootstrap.Modal(modalLogin);
 let toggles = document.querySelectorAll(".toggles");
-let btnAdmin = document.getElementById("btnAdmin")
+let btnAdmin = document.getElementById("btnAdmin");
 
 // Borrar datos storage
 
 function borrarDatos() {
-//   localStorage.clear();
+  // localStorage.clear();
   sessionStorage.clear();
 }
 
@@ -34,8 +39,8 @@ function validarUsuario(usersDB, user, pass) {
 
 function guardarDatos(usuarioDB, storage) {
   const usuario = {
-    name: usuarioDB.nombre,
-    user: usuarioDB.mail,
+    name: usuarioDB.name,
+    user: usuarioDB.user,
     pass: usuarioDB.pass,
   };
 
@@ -54,20 +59,27 @@ function presentarInfo(array, clase) {
   });
 }
 
-function funcionesAdmin(usuario){
-  if(usuario.tipo == "ADMINISTRADOR" ){
-    btnAdmin.className = "nav-item dropdown"
-  }else{
-    btnAdmin.className = "nav-item dropdown d-none"
+function funcionesAdmin(usuario) {
+  if (usuario.tipo == "ADMINISTRADOR") {
+    btnAdmin.className = "nav-item dropdown";
+  } else {
+    btnAdmin.className = "nav-item dropdown d-none";
   }
 }
 
-function estaLogueado(usuario) {
+function userlogin(usuario) {
   if (usuario) {
     saludar(usuario);
     presentarInfo(toggles, "d-none");
     funcionesAdmin(usuario);
   }
+}
+
+// Recuperar datos storage
+
+function recuperarUsuario(storage) {
+  let usuarioEnStorage = JSON.parse(storage.getItem("usuario"));
+  return usuarioEnStorage;
 }
 
 // Evento click boton login
@@ -82,10 +94,10 @@ btnLogin.addEventListener("click", (e) => {
       alert(`Usuario y/o contraseña erróneos`);
     } else {
       guardarDatos(data, sessionStorage);
-      //Recién ahora cierro el cuadrito de login
       modal.hide();
-      //Muestro la info para usuarios logueados
-      estaLogueado(data);
+      usuarioLogeado = true;
+      sessionStorage.setItem("usuarioLogeado", usuarioLogeado);
+      userlogin(data);
     }
   }
 });
@@ -95,5 +107,9 @@ btnLogin.addEventListener("click", (e) => {
 btnLogout.addEventListener("click", () => {
   borrarDatos();
   presentarInfo(toggles, "d-none");
-  btnAdmin.className = "nav-item dropdown d-none"
+  btnAdmin.className = "nav-item dropdown d-none";
+  usuarioLogeado = false;
+  sessionStorage.setItem("usuarioLogeado", usuarioLogeado);
 });
+
+userlogin(recuperarUsuario(sessionStorage));
