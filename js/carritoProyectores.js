4b63079c9cfd0fb3ac88798c076dd8c9e285c0c0
Juan Pablo Calabro <juampicalabro97@gmail.com>
2022-12-27 23:11:51 -0300

    Ultimos cambios

--- js/carritoProyectores.js
+++ js/carritoProyectores.js
@@ -1,13 +1,12 @@
 let proyectoresStock = proyectores.slice(0);
 let proyectoresReservados = [];
-let carritoActual = [];
+let carritoActual = [{"VIEW3", "VIEWSONIC", "MOD3", "SN101", 1, 0}];
 
 let elementosCarrito = 0;
 
 localStorage.setItem("proyectoresStock",JSON.stringify(proyectoresStock));
 localStorage.setItem("proyectoresReservados",JSON.stringify(proyectoresReservados));
-sessionStorage.setItem("elementosCarrito",JSON.stringify(elementosCarrito));
-
+sessionStorage.setItem("carritoActual",JSON.stringify(elementosCarrito));
 
 // Evento boton agregar carrito
 
@@ -18,32 +17,38 @@ function generateBTNaddID() {
       let elementos = boton.id.split("-");
       let btnAddID = elementos[1];
       elementosCarrito++;
-      actulizarBTNCarrito(elementosCarrito);
       actualizarProyectoresStock(btnAddID);
+      actulizarBurbujaCarrito();
       generateCatalogo(proyectoresStock);
       generateBTNaddID();
     });
   });
 }
-
-function actulizarBTNCarrito(elementos) {
-  let itemsCarrito = document.querySelector("#burbujaCarrito");
-  if (elementos == 0) {
-    itemsCarrito.innerHTML = "";
-    itemsCarrito.class = "badge rounded-pill bg-warning d-none";
-  } else {
-    itemsCarrito.class = "badge rounded-pill bg-warning";
-    itemsCarrito.innerHTML = `<p>${elementos}</p>`;
-  }
-}
-
 function actualizarProyectoresStock(itemID) {
   let instrumento = proyectoresStock.find((el) => el.cod_rec == itemID);
-  proyectoresReservados.push(instrumento);
+  carritoActual.push(instrumento);
+  sessionStorage.setItem("carritoActual",JSON.stringify(carritoActual));
   position = proyectoresStock.indexOf(instrumento);
   proyectoresStock.splice(position, 1);
 }
 
+function actulizarBurbujaCarrito() {
+  let carrito = recuperarCarrito();
+  let itemsCarrito = document.querySelector("#burbujaCarrito");
+  if (elementosCarrito == 0) {
+    itemsCarrito.innerHTML = "";
+    itemsCarrito.class = "badge rounded-pill bg-warning d-none";
+  } else {
+    itemsCarrito.class = "badge rounded-pill bg-warning";
+    itemsCarrito.innerHTML = `<p>${carrito.length}</p>`;
+  }
+}
+
+function recuperarCarrito() {
+  let carritoActual = JSON.parse(sessionStorage.getItem("carritoActual"));
+  return carritoActual;
+}
+
 window.addEventListener("load", function () {
-  actulizarBTNCarrito(elementosCarrito);
+  actulizarBurbujaCarrito();
 });
