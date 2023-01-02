b9687364ff72a9f71a49ae0ce5550ed3d894469c
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-01-01 21:10:59 -0300

    agregar proyectores

---
+++ js/agregar.js
@@ -0,0 +1,59 @@
+let Input_cod_rec = document.getElementById("Input-cod_rec");
+let Input_marca = document.getElementById("Input-marca");
+let Input_modelo = document.getElementById("Input-modelo");
+let Input_sn = document.getElementById("Input-sn");
+let checkbox_hdmi = document.getElementById("checkbox-hdmi");
+let checkbox_vga = document.getElementById("checkbox-vga");
+let btnAdd = document.getElementById("btnAdd");
+
+btnAdd.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    !Input_cod_rec.value ||
+    !Input_marca.value ||
+    !Input_modelo.value ||
+    !Input_sn.value
+  ) {
+    alert("Todos los campos son requeridos");
+  } else {
+    let hdmi = 0;
+    let vga = 0;
+    if (checkbox_hdmi.checked) {
+      hdmi = 1;
+    }
+    if (checkbox_vga.checked) {
+      vga = 1;
+    }
+    new_proyector = new Proyector(
+      Input_cod_rec.value,
+      Input_marca.value,
+      Input_modelo.value,
+      Input_sn.value,
+      hdmi,
+      vga,
+      "DISPONIBLE"
+    );
+
+    let arrayProyectoresStock = JSON.parse(
+      localStorage.getItem("proyectoresStock")
+    );
+    if (arrayProyectoresStock == null) {
+      let arrayProyectores = JSON.parse(
+        localStorage.getItem("proyectores")
+      );
+      arrayProyectores.push(new_proyector);
+      arrayProyectoresStock = arrayProyectores.slice(0) 
+      localStorage.setItem("proyectores", JSON.stringify(arrayProyectores));
+      localStorage.setItem("proyectoresStock", JSON.stringify(arrayProyectoresStock));
+    } else {
+        let arrayProyectores = JSON.parse(
+            localStorage.getItem("proyectores")
+          );
+        arrayProyectores.push(new_proyector)
+        arrayProyectoresStock.push(new_proyector)
+        localStorage.setItem("proyectores", JSON.stringify(arrayProyectores));
+        localStorage.setItem("proyectoresStock", JSON.stringify(arrayProyectoresStock));
+    }
+    alert("Item agregado correctamente");
+  }
+});
