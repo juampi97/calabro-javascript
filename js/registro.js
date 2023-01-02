8a1774c863dafb779964282313b87859b17565b0
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-01-01 23:26:20 -0300

    Registro usuarios

--- js/registro.js
+++ js/registro.js
@@ -1,25 +1,32 @@
-let input_name = document.getElementById('Input-name')
-let input_user = document.getElementById('Input-user')
-let input_pass = document.getElementById('Input-pass')
-let input_pass2 = document.getElementById('Input-pass2')
-let btnAdd = document.getElementById('btnAdd')
+let input_name = document.getElementById("Input-name");
+let input_user = document.getElementById("Input-user");
+let input_pass = document.getElementById("Input-pass");
+let input_pass2 = document.getElementById("Input-pass2");
+let btnAdd = document.getElementById("btnAdd");
 
-btnAdd.addEventListener('click',(e) => {
-    e.preventDefault();
-    if(!input_name.value || !input_user.value || !input_pass.value || !input_pass2.value){
-        alert("Todos los campos son requeridos");
-    }else{
-        if(input_pass.value != input_pass2.value){
-            alert("Las contraseñas ingresadas no coinciden")
-        }
-        else{
-            new_user = new Usuario(input_name.value, input_user.value, input_pass.value, "docente")
-            let arrayUsuarios = JSON.parse(
-                localStorage.getItem("usuariosBD")
-              );
-              arrayUsuarios.push(new_user);
-              localStorage.setItem("usuariosBD", JSON.stringify(arrayUsuarios));
-            alert("Registro exitoso")
-        }
+btnAdd.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    !input_name.value ||
+    !input_user.value ||
+    !input_pass.value ||
+    !input_pass2.value
+  ) {
+    alert("Todos los campos son requeridos");
+  } else {
+    if (input_pass.value != input_pass2.value) {
+      alert("Las contraseñas ingresadas no coinciden");
+    } else {
+      new_user = new Usuario(
+        input_name.value,
+        input_user.value,
+        input_pass.value,
+        "docente"
+      );
+      let arrayUsuarios = JSON.parse(localStorage.getItem("usuariosBD"));
+      arrayUsuarios.push(new_user);
+      localStorage.setItem("usuariosBD", JSON.stringify(arrayUsuarios));
+      alert("Registro exitoso");
     }
-})
+  }
+});
