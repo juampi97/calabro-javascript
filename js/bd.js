2e514b64cea3f35aa81727af7afbfafae85b4ab5
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-01-18 20:50:09 -0300

    Fetch JSON

--- js/bd.js
+++ js/bd.js
@@ -8,44 +8,6 @@ class Proyector {
     this.hdmi = parseInt(hdmi);
     this.estado = estado.toUpperCase();
   }
-  salidaHDMI() {
-    if (this.hdmi) {
-      return "Disponible";
-    } else {
-      return "No disponible";
-    }
-  }
-  salidaVGA() {
-    if (this.vga) {
-      return "Disponible";
-    } else {
-      return "No disponible";
-    }
-  }
-}
-class Notebook {
-  constructor(cod_rec, marca, modelo, sn, vga, hdmi) {
-    this.cod_rec = cod_rec.toUpperCase();
-    this.marca = marca.toUpperCase();
-    this.modelo = modelo.toUpperCase();
-    this.sn = sn.toUpperCase();
-    this.vga = parseInt(vga);
-    this.hdmi = parseInt(hdmi);
-  }
-  salidaHDMI() {
-    if (this.hdmi) {
-      return "Disponible";
-    } else {
-      return "No disponible";
-    }
-  }
-  salidaVGA() {
-    if (this.vga) {
-      return "Disponible";
-    } else {
-      return "No disponible";
-    }
-  }
 }
 
 class Usuario {
@@ -57,34 +19,45 @@ class Usuario {
   }
 }
 
-//Simulo BD
+//Simulo BD proyectores
 
-const usuarios = [
-  new Usuario("Juan Pablo", "juampi", "12345", "administrador"),
-  new Usuario("Nicolas", "nicolas", "12345", "docente"),
-];
+let proyectores = [];
 
-// const proyectores = [
-//   new Proyector("VIEW1", "VIEWSONIC", "MOD1", "SN001", 1, 0),
-//   new Proyector("VIEW2", "VIEWSONIC", "MOD1", "SN002", 0, 1),
-//   new Proyector("BENQ1", "BENQ", "MOD2", "SN003", 1, 0),
-//   new Proyector("BENQ2", "BENQ", "MOD3", "SN004", 1, 1),
-// ];
-const proyectoresStock = [
-  new Proyector("VIEW1", "VIEWSONIC", "MOD1", "SN001", 1, 0,"DISPONIBLE"),
-  new Proyector("VIEW2", "VIEWSONIC", "MOD1", "SN002", 0, 1,"DISPONIBLE"),
-  new Proyector("BENQ1", "BENQ", "MOD2", "SN003", 1, 0,"DISPONIBLE"),
-  new Proyector("BENQ2", "BENQ", "MOD3", "SN004", 1, 1,"DISPONIBLE"),
-];
+const pedirProyectores = async () => {
+  const resp = await fetch("/proyectores.json");
+  const data = await resp.json();
+  data.forEach((prod) => {
+    let newProyector = new Proyector(
+      prod.cod_rec,
+      prod.marca,
+      prod.modelo,
+      prod.sn,
+      prod.vga,
+      prod.hdmi,
+      prod.estado
+    );
+    proyectores.push(newProyector);
+  });
+};
+pedirProyectores();
 
-let proyectores = proyectoresStock.slice(0)
+//Simulo BD usuarios
 
-const notebooks = [
-  new Notebook("CR2", "CR", "MOD1", "SN006", 0, 1),
-  new Notebook("CR1", "CR", "MOD1", "SN005", 1, 0),
-  new Notebook("LENOVO1", "LENOVO", "MOD7", "SN003", 1, 0),
-  new Notebook("LENOVO2", "LENOVO", "MOD8", "SN004", 1, 1),
-];
+let usuarios = [];
+
+const pedirUsuarios = async () => {
+  const resp = await fetch("/usuarios.json");
+  const data = await resp.json();
+  data.forEach((user) => {
+    let newUsuario = new Usuario(user.name, user.user, user.pass, user.tipo);
+    usuarios.push(newUsuario);
+  });
+
+  //Subir al localST
+};
+pedirUsuarios();
+
+let proyectoresStock = proyectores.slice(0);
 
 // Genero arrays para forms Proyectores
 
@@ -100,28 +73,12 @@ proyectoresStock.forEach((elemento) => {
   codrecProyectores.push(elemento.cod_rec);
 });
 
-// Genero arrays para forms Notebooks
-
-let marcaNotebooks = [];
-let codrecNotebooks = [];
-
-notebooks.forEach((elemento) => {
-  marcaNotebooks.push(elemento.marca);
-});
-marcaNotebooks = [...new Set(marcaNotebooks)];
-
-notebooks.forEach((elemento) => {
-  codrecNotebooks.push(elemento.cod_rec);
-});
-
-// Agrego BD al storage
+// // Agrego BD al storage
 
 window.addEventListener("load", function () {
-  let arrayUsuarios = JSON.parse(
-    localStorage.getItem("usuariosBD")
-  );
-  if(arrayUsuarios == null){
+  let arrayUsuarios = JSON.parse(localStorage.getItem("usuariosBD"));
+  if (arrayUsuarios == null) {
     localStorage.setItem("usuariosBD", JSON.stringify(usuarios));
   }
   localStorage.setItem("proyectores", JSON.stringify(proyectores));
-})
+});
