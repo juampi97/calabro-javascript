3c8fcd80f36293060411c83a681c0e3d796514c8
Juan Pablo Calabro <juampicalabro97@gmail.com>
2023-01-19 23:59:59 -0300

    Alta reserva

--- js/reservar.js
+++ js/reservar.js
@@ -1,26 +1,25 @@
 formDia = document.getElementById("selectDateReserva");
 formHorario = document.getElementById("selectHorarioReserva");
 
-let usuario = recuperarUsuario(sessionStorage);
+btnReservarCarrito.addEventListener("click", () => {
+  let usuario = recuperarUsuario(sessionStorage);
 
-btnReservarCarrito.addEventListener('click',() => {
+  day = formDia.value;
+  vectorDay = day.split("-");
+  dia = `${vectorDay[2]}/${vectorDay[1]}/${vectorDay[0]}`;
+  horario = formHorario.value;
 
-day = formDia.value
-vectorDay = day.split('-')
-dia = `${vectorDay[2]}/${vectorDay[1]}/${vectorDay[0]}` 
-horario = formHorario.value
-
-if(horario == "null" || day == "" ){
+  if (horario == "null" || day == "") {
     Swal.fire(
-        '',
-        'Seleccione la fecha en la que desea realizar la reserva',
-        'warning'
-      )
-}else{
+      "",
+      "Seleccione la fecha en la que desea realizar la reserva",
+      "warning"
+    );
+  } else {
     Swal.fire(
-        '',
-        `${usuario.name}, se realizó la reserva para el dia ${dia} en el turno de ${horario}.`,
-        'success'
-      )
-}
-})
+      "",
+      `${usuario.name}, se realizó la reserva para el dia ${dia} en el turno de ${horario}.`,
+      "success"
+    );
+  }
+});
